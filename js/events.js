6b912c93306782a4fa65905bf8518764a5b2fc75
George Lugo <stg77713@gmail.com>
2020-03-15 19:57:06 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,13 +14,16 @@ function pressIt(){
   return $('input').on('keydown',
   function(key){
     if(key === 'G'){
-      alert('G was pressed')
+      alert('G was pressed');
     }
-  })
+  });
 }
 
 function submitIt(){
-  
+  return $('form').on('submit',
+  function(key){
+    alert("Your form is going to be submitted now.")
+  })
 }
 $(document).ready(function(){
 
