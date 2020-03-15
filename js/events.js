d1cdf4aa1fbb45cea4f269c1d3c338351508c59b
George Lugo <stg77713@gmail.com>
2020-03-15 19:49:53 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,11 @@
 //define functions here
+function getIt(){
+  $(p).on('click', window.alert("Hey!"));
+}
 
+function frameIt(){
+  $('img').on('load', )
+}
 $(document).ready(function(){
 
 // call functions here
