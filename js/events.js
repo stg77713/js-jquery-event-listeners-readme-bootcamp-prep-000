43351bf1929f5ea02ef8006aa7ddf6f9bc64b856
George Lugo <stg77713@gmail.com>
2020-03-15 20:02:26 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 function pressIt(){
   return $('input').on('keydown',
   function(key){
-    if(key.which === '71'){
+    if(key.which == 71){
       alert('G was pressed');
     }
   });
